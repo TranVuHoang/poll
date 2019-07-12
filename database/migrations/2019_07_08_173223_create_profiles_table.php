38bd192e2a4d049b70b12a864a05fb508d08feec
TranVuHoang <35668249+TranVuHoang@users.noreply.github.com>
2019-07-12 11:03:43 +0700

    Update 2019_07_08_173223_create_profiles_table.php

--- database/migrations/2019_07_08_173223_create_profiles_table.php
+++ database/migrations/2019_07_08_173223_create_profiles_table.php
@@ -15,6 +15,7 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name')';
             $table->timestamps();
         });
     }
